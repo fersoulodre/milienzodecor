e283ce07850e168e6d45a905f41bc31618ceeb6a
Fernando Antonio Soulodre Tejero <fsoulodre@gmail.com>
2026-07-29 19:53:17 -0400

    inventario14

--- components/ProductDetailClient.tsx
+++ components/ProductDetailClient.tsx
@@ -123,7 +123,7 @@ export default function ProductDetailClient({ producto }: { producto: Producto }
                 <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
-                ¡Apúrate! Solo quedan {producto.stock} unidades de esta obra.
+                Quedan {producto.stock} unidades de esta obra.
               </p>
             )}
 
